7148c451a99538c0a87b4c32e1bdff54d15f9646
jonathanraya <jonathanraya79@gmail.com>
2023-12-06 18:22:51 -0600

    Begin api endpoints

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,15 @@
 class PetsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Pets", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    Pet.create(
+            name: 'Jazz',
+            age: 3,
+            enjoys: 'Sleeping and messing with her younger brother',
+            image: 'https://pixabay.com/photos/cat-russian-blue-gray-cat-2761187/'
+    )
+
+    get '/pets'
+
+    pet = JSON.parse(response.body)
+    expect(response).to have_http_status(200)
+    expect(pet.length).to eq 1
+    end
   end
 end
