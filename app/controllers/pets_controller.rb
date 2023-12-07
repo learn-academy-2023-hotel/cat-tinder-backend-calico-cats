e32fff85df63a14cbf0160e36e465de8c71bcab3
zeniavarela <zvarela8@gmail.com>
2023-12-07 11:23:20 -0800

    api endpoints

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,15 +1,31 @@
 class PetsController < ApplicationController
 
-  def index
+  def index 
+    pets = Pet.all
+    render json: pets 
   end
 
   def create
+    pet = Pet.create(pet_params)
+    render json: pet
   end
 
-  def update
+  def update 
+    pet = Pet.find(params[:id])
+    pet.update(pet_params)
+    render json: pet 
   end
 
   def destroy
+    pet = Pet.find(params[:id])
+    pet.destroy
+    render json: pet
   end
   
+
+  # strong params
+  private
+  def pet_params
+    params.require(:pet).permit(:name, :age, :enjoys, :image)
+  end
 end
--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -2,19 +2,97 @@ require 'rails_helper'
 
 RSpec.describe "Pets", type: :request do
   describe "GET /index" do
-    it'gets a lsit of pets' do
+    it 'gets a lsit of pets' do
     Pet.create(
-            name: 'Jazz',
-            age: 3,
-            enjoys: 'Sleeping and messing with her younger brother',
-            image: 'https://pixabay.com/photos/cat-russian-blue-gray-cat-2761187/'
+            name: 'Felix',
+            age: 4,
+            enjoys: 'Meow Mix',
+            image: 'http.//xxxxxxx'
     )
 
     get '/pets'
 
     pet = JSON.parse(response.body)
     expect(response).to have_http_status(200)
-    expect(pet.length).to eq 1
+    expect(pet.length).to eq 2
     end
   end
+  describe ' POST / create ' do
+    it 'creates a new pet' do 
+      pet_params = {
+        pet: {
+            name: 'Felix',
+            age:4,
+            enjoys: 'Meow Mix',
+            image: 'http.//xxxxxxx'
+            }
+        }
+
+     post '/pets', params: pet_params
+
+     expect(response).to have_http_status(200)
+     pet = Pet.last
+
+     expect(pet.name).to eq 'Felix'
+      expect(pet.age).to eq 4
+      expect(pet.enjoys).to eq 'Meow Mix'
+      expect(pet.image).to eq 'http.//xxxxxxx'
+  end
 end
+
+  describe 'PATCH/ update' do
+    it 'updates a pet' do
+      pet_params = {
+        pet: {
+            name: 'Felix',
+            age:4,
+            enjoys: 'Meow Mix',
+            image: 'http.//xxxxxxx'
+            }
+        }
+        post '/pets', params: pet_params
+        pet = Pet.last
+
+        updated_params = {
+          pet: {
+            name: 'Buster',
+            age: 4,
+            enjoys: 'Meow Mix',
+            image: 'http.//xxxxxxx'
+          }
+        }
+
+        patch "/pets/#{pet.id}", params: updated_params
+
+        updated_pet = Pet.find(pet.id)
+        expect(response).to have_http_status(200)
+        expect(updated_pet.name).to eq "Buster"
+      end
+    end
+
+    describe 'DELETE / pets/:id' do 
+      it 'deletes a pet' do
+        pet_params = {
+          pet: {
+              name: 'Felix',
+              age:4,
+              enjoys: 'Meow Mix',
+              image: 'http.//xxxxxxx'
+              }
+          }
+          post '/pets', params: pet_params
+          pet = Pet.last
+      
+
+          delete "/pets/#{pet.id}"
+  
+      expect(response).to have_http_status(200)
+      deleted_pet = Pet.find_by(id: pet.id)
+      expect(deleted_pet).to be_nil 
+      end
+    end
+
+
+
+end
+
