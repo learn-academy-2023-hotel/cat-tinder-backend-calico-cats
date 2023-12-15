4fa4d79d37449f1cd7520d9ce8f781a1ed948bf2
zeniavarela <zvarela8@gmail.com>
2023-12-15 11:28:54 -0800

    files for deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # <- change this to allow requests from any domain while in development.
+    origins 'pet-tinder-frontend.onrender.com'  # <- change this to allow requests from any domain while in development.
 
     resource '*',
       headers: :any,
